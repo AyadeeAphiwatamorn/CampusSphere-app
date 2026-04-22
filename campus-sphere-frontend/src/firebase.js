c77f36315f9a2dff93ae77203379ed093223cbfc
Ayadee Aphiwatamorn <ayadee.aphiwatamorn@gmail.com>
2026-04-23 00:41:40 +0530

    added env security

--- campus-sphere-frontend/src/firebase.js
+++ campus-sphere-frontend/src/firebase.js
@@ -1,23 +1,9 @@
-// Import the functions you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-// ✅ ADD THIS LINE
-import { getAuth } from "firebase/auth";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDbl2LZr3KGSGMZN1ow6eWiyumO_a4RM9I",
-  authDomain: "campussphere-4b7a4.firebaseapp.com",
-  projectId: "campussphere-4b7a4",
-  storageBucket: "campussphere-4b7a4.firebasestorage.app",
-  messagingSenderId: "615732178955",
-  appId: "1:615732178955:web:ee68d330e09105e01f570e",
-  measurementId: "G-VECHMLZDG0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// ✅ THIS IS THE MAIN FIX
-export const auth = getAuth(app);
